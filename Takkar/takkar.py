88e334d70afd20a27cbcf001d2c72d7c3ae02ff6
NickHood08 <nicholashood08@gmail.com>
2026-09-13 18:02:12 -0400

    Create takkar.py

---
+++ Takkar/takkar.py
@@ -0,0 +1,9 @@
+trumpsize = int(input())
+kimsize = int(input())
+
+if trumpsize > kimsize:
+    print("MAGA!")
+elif kimsize > trumpsize:
+    print("FAKE NEWS!")
+else: 
+    print("WORLD WAR 3!")
